56d0bba0cf1e4dabf98a2e511bd347f3cfc971e2
Vladislav Kotov <finrerty@gmail.com>
2020-02-13 00:16:25 +0300

    Fixed packer tests in Travis CI

--- play-travis/tests.sh
+++ play-travis/tests.sh
@@ -4,6 +4,7 @@ sudo pip install --upgrade pip
 sudo pip install ansible
 
 #packer
-packer validate -var-file=infra/packer/variables.json.example infra/packer/app.json
-packer validate -var-file=infra/packer/variables.json.example infra/packer/db.json
-packer validate -var-file=infra/packer/variables.json.example infra/packer/ui.json
+cd infra
+packer validate -var-file=packer/variables.json.example packer/app.json
+packer validate -var-file=packer/variables.json.example packer/db.json
+packer validate -var-file=packer/variables.json.example packer/ui.json
