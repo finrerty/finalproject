a092e51123617b85caf223885ceb57ba2a7a787d
Vladislav Kotov <finrerty@gmail.com>
2020-02-13 00:24:13 +0300

    Added tests for Terraform in Travis CI

--- play-travis/tests.sh
+++ play-travis/tests.sh
@@ -8,3 +8,14 @@ cd infra
 packer validate -var-file=packer/variables.json.example packer/app.json
 packer validate -var-file=packer/variables.json.example packer/db.json
 packer validate -var-file=packer/variables.json.example packer/ui.json
+
+#install terraform & tflint
+curl https://releases.hashicorp.com/terraform/0.12.8/terraform_0.12.8_linux_amd64.zip -o /tmp/terraform.zip
+sudo unzip /tmp/terraform.zip terraform -d /usr/bin/
+curl https://raw.githubusercontent.com/wata727/tflint/master/install_linux.sh | bash
+
+#terraform stage
+cd terraform/stage
+terraform get && terraform init
+tflint
+terraform validate
